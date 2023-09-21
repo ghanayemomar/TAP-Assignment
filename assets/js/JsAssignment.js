d2a8c83bf252df6358c77ecdd2fff7a979fd1a06
ghanayemomar <o.ghanaiem@student.aaup.edu>
2023-09-21 04:06:25 +0300

    init

---
+++ assets/js/JsAssignment.js
@@ -0,0 +1,16 @@
+function twoSum(nums, target) {
+  const numIndexMap = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (complement in numIndexMap) {
+      return [numIndexMap[complement], i];
+    }
+    numIndexMap[nums[i]] = i;
+  }
+}
+
+const nums = [2, 7, 11, 15];
+const target = 9;
+const result = twoSum(nums, target);
+console.log(result);
